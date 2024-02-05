1e1092d9d9d2de11d06e125b15fa95154e8a5ffa
Sanidhya-30 <sanidhya3009@gmail.com>
2024-02-05 12:23:56 +0530

    working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include<string.h>
+#include <string.h>
 
 using namespace cv;
 using namespace std;
@@ -8,15 +8,15 @@ using namespace std;
 
 // ----------------------------- SOME CONSTANTS  -------------------------
 
-Point mouseClick;
-int radius = 2;
+int radius = 50;
 int font = FONT_HERSHEY_SIMPLEX;
 cv::Scalar color_text = cv::Scalar(0, 255, 0); //green
 cv::Scalar color_circle = cv::Scalar(0, 255, 255); //yellow
-int thickness = 3;
-String lock_status = "LOCKED";
-int frame_heigth = 480;
-int frame_width = 640;
+int thickness = 1;
+String lock_status = "LOOKING";
+int FH = 480 ;  // frame heigth
+int FW = 640 ;  // frame width
+Point mouseClick = Point((FW/2), (FH/2));
 
 // ----------------------------- MOUSECLICK CALLBACK  -------------------------
 
@@ -25,9 +25,25 @@ void onMouseClick(int event, int x, int y, int flags, void* userdata)
     if (event == EVENT_LBUTTONDOWN) 
     {
         mouseClick = Point(x, y);
-      //   lock_status = "LOCKED";
+        lock_status = "LOCKED";
     }
    
+   // return;
+}
+
+
+
+// ----------------------------- CLEAR CIRCLE  -------------------------
+
+void clear_screen(cv::Mat frame)
+{
+   //clear screen
+   frame = Mat::zeros(frame.size(), frame.type());
+   cv::imshow("Display Window", frame);
+   mouseClick = Point((FW/2), (FH/2));;
+   cout<< "Target out of bounds";
+   lock_status = "LOOKING";
+   // cal_fps;
    return;
 }
 
@@ -48,10 +64,10 @@ void draw_circle(cv::Mat frame, int radius, cv::Scalar color_circle, int thickne
 void frame_text(cv::Mat frame, double fps, int radius, cv::Scalar color_text, int thickness)
 {
    //locked
-   putText(frame, lock_status, Point((frame_width/2), (frame_heigth/2)), font, 1, color_text, thickness);
+   putText(frame, lock_status, Point(((FW/2)-60),(((FH))-50)), font, 1, color_text, (thickness+1));
 
    //line
-   Point p1(mouseClick.x, 0), p2(frame_width, mouseClick.y), p3(mouseClick.x, frame_heigth), p4(0, mouseClick.y);
+   Point p1(mouseClick.x, 0), p2(FW, mouseClick.y), p3(mouseClick.x, FH), p4(0, mouseClick.y);
    line(frame, p1, p3, Scalar(255, 0, 0), thickness, LINE_4); 
    line(frame, p2, p4, Scalar(255, 0, 0), thickness, LINE_4); 
    return;
@@ -68,13 +84,13 @@ double cal_fps(cv::Mat frame, double fps)
    double elapsedSeconds = chrono::duration_cast<chrono::duration<double>>(currentTime - lastTime).count();
    
    // Update FPS using weighted average
-   double currentFPS = (0.85 / elapsedSeconds) + ((1 - 0.85) * currentFPS);
+   fps = (0.85 / elapsedSeconds) + ((1 - 0.85) * fps);
    lastTime = currentTime;
 
    //fps
-   putText(frame, "FPS: " + to_string(static_cast<int>(fps)), Point(10, 30), font, 1, color_text, thickness);
+   putText(frame, "FPS: " + to_string(static_cast<int>(fps)), Point(10, 30), font, 1, color_text, (thickness+1));
    
-   return currentFPS;
+   return fps;
 }
 
 
@@ -92,8 +108,9 @@ int main()
       return -1;
    }
 
-   int frame_width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
-   int frame_height = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
+   // FW = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
+   // FH = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
+   cout<<FW<<" "<<FH;
 
    cv::namedWindow("Display Window");
    setMouseCallback("Display Window", onMouseClick, NULL);
@@ -106,15 +123,17 @@ int main()
       cap >> frame;
 
       fps = cal_fps(frame, fps);
+      frame_text(frame, fps, radius, color_text, thickness);
+      draw_circle(frame, radius, color_circle, thickness);
+
+
+      if (!mouseClick.inside(Rect(50, 50, (FW-100), (FH-100)))) //checks if click in bound
+      {
+         clear_screen(frame);
+         
+      }
 
       cv::imshow("Display Window", frame);
-
-        if (!mouseClick.inside(Rect(0, 0, (frame.cols-20), (frame.rows-20)))) //checks if click in bound
-        {
-            frame_text(frame, fps, radius, color_text, thickness);
-            draw_circle(frame, radius, color_circle, thickness);
-        }
-
       // Check for user input to exit the loop
       if (cv::waitKey(1) == 27) // 'Esc' key
          break;
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cd
 cd LockIn/
 cmake .
 make
-
+./main
